9e30d32f536fa7f1d406366a1f7a97c29a9d1bf4
Ajay H. Mota <ajayhmota@gmail.com>
2019-06-21 19:50:09 +0200

    First commit

---
+++ controllers/router.js
@@ -0,0 +1,35 @@
+$( document ).ready(function() {
+    $('.root').load("views/home.html");
+});
+
+$(".home").click(function(){
+   $('.root').load("views/home.html");
+});
+
+$(".community").click(function(){
+   $('.root').load("views/community.html");
+});
+
+$(".consult").click(function(){
+   $('.root').load("views/consult.html");
+});
+
+$(".monitor").click(function(){
+   $('.root').load("views/monitor.html");
+});
+
+$(".games").click(function(){
+   $('.root').load("views/games.html");
+});
+
+$(".messaging").click(function(){
+   $('.root').load("views/messaging.html");
+});
+
+$(".profile").click(function(){
+   $('.root').load("views/profile.html");
+});
+
+$(".settings").click(function(){
+   $('.root').load("views/settings.html");
+});
