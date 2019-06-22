4f885d619f5c1172a194cc1f08ca5c648bfc7bd1
Ajay H. Mota <ajayhmota@gmail.com>
2019-06-22 15:42:15 +0200

    pushed sidenav

---
+++ controllers/nav.js
@@ -0,0 +1,11 @@
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("my-nav-overlay").style.width = "100%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("my-nav-overlay").style.width = "0";
+} 
