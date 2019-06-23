eb87ea0e03bd6eb1f80105fcfe55345e17d46be9
Ajay H. Mota <ajayhmota@gmail.com>
2019-06-23 14:06:35 +0200

    messaging has been implemented

--- controllers/router.js
+++ controllers/router.js
@@ -1,35 +1,44 @@
 $( document ).ready(function() {
     $('.root').load("views/home.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".home").click(function(){
    $('.root').load("views/home.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".community").click(function(){
    $('.root').load("views/community.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".consult").click(function(){
    $('.root').load("views/consult0.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".monitor").click(function(){
    $('.root').load("views/monitor.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".games").click(function(){
    $('.root').load("views/games.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".messaging").click(function(){
    $('.root').load("views/messaging.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".profile").click(function(){
    $('.root').load("views/profile.html");
+    $('.footer').load("views/main-footer.html");
 });
 
 $(".settings").click(function(){
    $('.root').load("views/settings.html");
+    $('.footer').load("views/main-footer.html");
 });
