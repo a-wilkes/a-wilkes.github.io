29a7387c126e62f3f01642dbe70d03ed52d5d743
Ajay H. Mota <ajayhmota@gmail.com>
2019-06-23 16:46:13 +0200

    Increased header font size

--- controllers/router.js
+++ controllers/router.js
@@ -1,49 +1,49 @@
 $(".home").click(function(){
    $('.root').load("views/home.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("BabyBreathe");
+    $('h4.title-txt').text("BabyBreathe");
 });
 
 $(".community").click(function(){
    $('.root').load("views/community.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Community");
+    $('h4.title-txt').text("Community");
 });
 
 $(".consult").click(function(){
    $('.root').load("views/consult0.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Consultations");
+    $('h4.title-txt').text("Consultations");
 });
 
 $(".monitor").click(function(){
    $('.root').load("views/monitor.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Monitor");
+    $('h4.title-txt').text("Monitor");
 });
 
 $(".games").click(function(){
    $('.root').load("views/games.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Recreation");
+    $('h4.title-txt').text("Recreation");
 });
 
 $(".messaging").click(function(){
    $('.root').load("views/messaging.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Messaging");
+    $('h4.title-txt').text("Messaging");
 });
 
 $(".profile").click(function(){
    $('.root').load("views/profile.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Profile");
+    $('h4.title-txt').text("Profile");
 });
 
 $(".settings").click(function(){
    $('.root').load("views/settings.html");
     $('.footer').load("views/main-footer.html");
-    $('h5.title-txt').text("Settings");
+    $('h4.title-txt').text("Settings");
 });
 
 $(".fa-bars").click(function(){
