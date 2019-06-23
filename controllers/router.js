93e7123d62ed2300b14e4d9886b213a75ec89fd6
Ajay H. Mota <ajayhmota@gmail.com>
2019-06-23 23:05:38 +0200

    more header title fixes

--- controllers/router.js
+++ controllers/router.js
@@ -1,55 +1,63 @@
 $(".home").click(function(){
    $('.root').load("views/home.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
-    $('h5.title-txt').text("BabyBreathe");
+    $('.header').load("views/main-header.html", function() {
+       $('h5.title-txt').text("BabyBreathe"); 
+    });
 });
 
 $(".community").click(function(){
    $('.root').load("views/community.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Community");
+        });
 });
 
 $(".consult").click(function(){
    $('.root').load("views/consult0.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Consultations");
+        });
 });
 
 $(".monitor").click(function(){
    $('.root').load("views/monitor.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Monitor");
+        });
 });
 
 $(".games").click(function(){
    $('.root').load("views/games.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Recreation");
+        });
 });
 
 $(".messaging").click(function(){
    $('.root').load("views/messaging.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Messaging");
+        });
 });
 
 $(".profile").click(function(){
    $('.root').load("views/profile.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Profile");
+        });
 });
 
 $(".settings").click(function(){
    $('.root').load("views/settings.html");
     $('.footer').load("views/main-footer.html");
-    $('.header').load("views/main-header.html");
+    $('.header').load("views/main-header.html", function() {
     $('h5.title-txt').text("Settings");
+        });
 });
