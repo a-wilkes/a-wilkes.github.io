f3d75ad83909da4dcaebf8714703728488c705d9
a-wilkes <a.sjwilkes@gmail.com>
2019-06-24 13:41:01 +0200

    Consultation css and js separated

---
+++ js/consult.js
@@ -0,0 +1,20 @@
+$(".c-schedule").click(function () {
+    $('.root').load("views/consultation/consult0.html");
+    $('.footer').load("views/main-footer.html");
+    $('.header').load("views/main-header.html", function () {
+        $('h5.title-txt').text("Consultation");
+    });
+});
+
+$(".schedule").click(function () {
+    $('.root').load("views/consultation/consult.html");
+    $('.header').load("views/consultation/header-back-consult.html");
+});
+
+$(".fa-arrow-left").click(function () {
+    $('.root').load("views/consultation/consult0.html");
+    $('.footer').load("views/main-footer.html");
+    $('.header').load("views/main-header.html", function () {
+        $('h5.title-txt').text("Consultation");
+    });
+});
